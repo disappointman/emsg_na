cd43f6fe2ac573ccf55fd87fb1730a30849c3219
Dherp Cuevas <34502161+dherpcuevas@users.noreply.github.com>
2018-02-05 10:50:03 +0800

    View Announcement

---
+++ ViewAnnouncement.php
@@ -0,0 +1,189 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>CREOTEC - Announcements</title>
+
+	<link rel="shortcut icon" type="image/x-icon" href="assets/images/icon.png">
+
+	<!-- Global stylesheets -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+	<link href="assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/core.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/components.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/colors.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/gallery-clean.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.css">
+	<!-- /global stylesheets -->
+
+	<!-- Core JS files -->
+	<script type="text/javascript" src="assets/js/plugins/loaders/pace.min.js"></script>
+	<script type="text/javascript" src="assets/js/core/libraries/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/core/libraries/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/loaders/blockui.min.js"></script>
+	<!-- /core JS files -->
+
+	<!-- Theme JS files -->
+	<script type="text/javascript" src="assets/js/plugins/forms/validation/validate.min.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/forms/selects/bootstrap_multiselect.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/forms/inputs/touchspin.min.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/forms/selects/select2.min.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/forms/styling/switch.min.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/forms/styling/switchery.min.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/forms/styling/uniform.min.js"></script>
+
+	<script type="text/javascript" src="assets/js/core/app.js"></script>
+	<script type="text/javascript" src="assets/js/pages/timelines.js"></script>
+
+
+	<!-- /theme JS files -->
+
+	<style type="text/css">
+		img {
+		    max-width: 100%;
+		    max-height: 100%;
+		    vertical-align: middle;
+
+		    padding-bottom: 20px;
+		}
+
+		.center{
+		    display:block;
+		    margin:auto;
+		}
+
+
+	</style>
+</head>
+
+<body>
+	<!-- Page container -->
+	<div class="page-container">
+
+		<!-- Page content -->
+		<div class="page-content">
+
+			<!-- Main content -->
+			<div class="content-wrapper">
+
+				<div class="panel-body">
+
+					
+					<div class="panel panel-white timeline-content">
+						<div class="panel-heading">
+							<h3 class="panel-title">Author</h3>
+							<div class="heading-elements">
+								<h3 class="panel-title text-muted">Date Time</h3>
+		                	</div>
+						</div>
+
+						<div class="panel-body">
+							
+
+
+							<div class="col-lg-6">
+								<h3 class="text-semibold">Title</h3>
+								<p class="content-group">Announcement Body</p>
+							</div>
+
+							<div class="col-lg-6">
+								<div class="gallery-container">
+							    
+								    <div class="tz-gallery">
+
+								        <div class="row">
+								        	<center>
+
+							        		<div class="col-sm-3 col-md-3">
+								                <a class="lightbox" href="assets/images/backgrounds/2.jpg">
+								                    <img src="assets/images/backgrounds/2.jpg">
+								                </a>
+								            </div>
+
+								            <div class="col-sm-3 col-md-3">
+								                <a class="lightbox" href="assets/images/backgrounds/3.jpg">
+								                    <img src="assets/images/backgrounds/3.jpg">
+								                </a>
+								            </div>
+
+								            <div class="col-sm-3 col-md-3">
+								                <a class="lightbox" href="assets/images/backgrounds/4.jpg">
+								                    <img src="assets/images/backgrounds/4.jpg">
+								                </a>
+								            </div>
+
+								            <div class="col-sm-3 col-md-3">
+								                <a class="lightbox" href="assets/images/backgrounds/3.jpg">
+								                    <img src="assets/images/backgrounds/3.jpg">
+								                </a>
+								            </div>
+
+								            <div class="col-sm-3 col-md-3">
+								                <a class="lightbox" href="assets/images/backgrounds/4.jpg">
+								                    <img src="assets/images/backgrounds/4.jpg">
+								                </a>
+								            </div>
+
+								            <div class="col-sm-3 col-md-3">
+								                <a class="lightbox" href="assets/images/backgrounds/3.jpg">
+								                    <img src="assets/images/backgrounds/3.jpg">
+								                </a>
+								            </div>
+
+								            <div class="col-sm-3 col-md-3">
+								                <a class="lightbox" href="assets/images/backgrounds/5.jpg">
+								                    <img src="assets/images/backgrounds/5.jpg">
+								                </a>
+								            </div>
+
+								            </center>
+
+								        </div>
+
+								    </div>
+
+								</div>
+							</div>
+
+						</div>
+
+						<div class="panel-footer ">
+							<div class="heading-elements">
+								<span class="heading-btn pull-right">
+									<a href="#" class="btn btn-link"><i class="icon-share2 position-right"></i> Share Link</a>
+								</span>
+							</div>
+						</div>
+
+
+					</div>
+
+					<!-- Load More -->
+			        <div class="row">
+					    <div class="text-center">
+					        <button class="btn btn-link">Go to Home...</button>
+					    </div>
+					</div>
+					<!-- Load More -->
+
+				</div>
+
+			</div>
+			<!-- Main content -->
+
+		</div>
+		<!-- Page content -->
+
+	</div>
+	<!-- Page container -->
+</body>
+
+</html>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+
+	<script>
+	    baguetteBox.run('.tz-gallery');
+	</script>
